71e09aa06da0ccc7aa25342fc0fd170ec3eb5111
Junninho Thomas <jthomas2016@bklawtech.com>
2015-10-21 21:24:50 +0000

    finished assignment without styling

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,18 +3,43 @@ var aiPoint = 0;
 
 // This function returns the selection of the computer
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    var random = Math.random();
+    if (random < 1/3) {
+        return 'rock';
+    }
+    
+    if (random < 2/3) {
+        return 'paper';
+    }
+    
+    return 'scissors';
 }
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
     //TODO: pick the correct winner: user or ai
     //TODO: Add one point for the winner
+    if (userValue === aiValue) {
+        return 'draw';
+    }
+    
+    if (
+        userValue ==='rock' && aiValue === 'scissors' || 
+        userValue ==='paper' && aiValue === 'rock' ||
+        userValue ==='scissors' && aiValue === 'paper'
+    ) {
+        userPoint++;
+        return 'user';
+    }
+    
+    aiPoint++;
+    return 'ai';
 }
 
 // This function sets the scoreboard with the correct points
 function setScore() {
-
+    $('#userPoint').text(userPoint);
+    $('#aiPoint').text(aiPoint);
 }
 
 // This function captures the click and picks the winner
@@ -23,6 +48,8 @@ function evaluate(evt) {
     var aiValue = getAISelection();
 
     var winner = pickWinner(userValue, aiValue);
+    
+    setScore();
 
     if ( 'user' === winner ) {
         $('#message').delay(50).text('You have won!, Click a box to play again');
@@ -35,5 +62,6 @@ function evaluate(evt) {
 
 // This function runs on page load
 $(document).ready(function(){
-
+    setScore()
+    $('.token').on('click', evaluate);
 });
